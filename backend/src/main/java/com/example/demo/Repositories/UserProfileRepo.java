b1e7aec5d69635071884b0c12ceb22cee3c2c4c7
MatasBrazauskas <matasbrazauskas123456@gmail.com>
2025-09-23 00:21:40 +0300

    Need to write an user log in functionality

--- backend/src/main/java/com/example/demo/Repositories/UserProfileRepo.java
+++ backend/src/main/java/com/example/demo/Repositories/UserProfileRepo.java
@@ -2,6 +2,8 @@ package com.example.demo.Repositories;
 
 import com.example.demo.Entities.UsersProfileInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
@@ -9,4 +11,7 @@ import java.util.Optional;
 public interface UserProfileRepo extends JpaRepository<UsersProfileInformation, Long>
 {
     Optional<UsersProfileInformation> findByEmail(String email);
+
+    @Query(value = "INSERT INTO UserProfile(email, name) VALUES($email, $name)", nativeQuery = true)
+    UsersProfileInformation save(@Param("name") final String name, @Param("email") final String email);
 }
